a16c56ed61df7be1f6031ae4cbe7b75fe480dfe3
Tom Kim <babooteng2@gmail.com>
2021-08-25 20:31:01 +0900

    #3.2 Getting The Tweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,28 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+const COLLECTION_NAME = "firebase-tweet";
 
 const Home = () => {
   const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+  const getTweets = async () => {
+    const querySnapshot = await dbService.collection(COLLECTION_NAME).get();
+    querySnapshot.forEach((doc) => {
+      // set 으로 시작하는 함수를 사용할 때 리액트는 이전의 값을 참조할 수 있게 해준다. [curr, prev]
+      const tweetObj = {
+        id: doc.id,
+        ...doc.data(),
+      };
+      setTweets((prev) => [tweetObj, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getTweets();
+  }, []);
   const onSubmit = async (e) => {
     e.preventDefault();
-    await dbService.collection("firebase-tweet").add({
+    await dbService.collection(COLLECTION_NAME).add({
       tweet,
       createAt: Date.now(),
     });
@@ -17,6 +34,7 @@ const Home = () => {
     } = e;
     setTweet(value);
   };
+  console.log(tweets);
   return (
     <>
       <form onSubmit={onSubmit}>
@@ -29,6 +47,9 @@ const Home = () => {
         />
         <input type="submit" value="tweet" />
       </form>
+      {tweets.map((tweet) => {
+        return <p key={tweet.id}>{tweet.tweet}</p>;
+      })}
     </>
   );
 };
